836174965a8a061590d9a5ec195df40b0fe4f10a
xuchen <chen.xu@geelink.cn>
2021-06-06 17:23:28 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-python train_keyword_prediction.py --config ./configs/keyword_prediction_config.json
+#python train_keyword_prediction.py --config ./configs/tj_keyword_prediction_config.json
+python train_retrieval.py --config ./configs/tj_retrieval_config.json
